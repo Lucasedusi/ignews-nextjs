33e2d2db38bc41e055eaaea482df04dfc7ee4099
lucasedusi <lucasmto.16@hotmail.com>
2022-05-25 18:23:57 -0300

    add static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -28,7 +28,7 @@ export default function Home({ product }: HomeProps) {
 					</h1>
 					<p>
 						Get access to all the publications <br />
-						<span>for {product.amount} month</span>
+						for <span>{product.amount}</span> month
 					</p>
 
 					<SubscribeButton priceId={product.priceId} />
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
 	);
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 	const price = await stripe.prices.retrieve("price_1L3QgYKBKCAfodOXaYowuhN7");
 
 	const product = {
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 		props: {
 			product,
 		},
+		revalidate: 60 * 60 * 24,
 	};
 };
